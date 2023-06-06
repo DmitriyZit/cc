7f232eab2379a5ca8e272764f1f002d5dad14d69
DmitriyZit <adima74@gmail.com>
2023-06-06 10:43:02 +0300

    Домашнее задание задача 36

--- Dom_5/Program.cs
+++ Dom_5/Program.cs
@@ -3,7 +3,7 @@
 /*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
-*/
+
 
 Console.Write("Введите нижнюю границу массива : ");
 int minValue = Convert.ToInt32(Console.ReadLine());
@@ -69,12 +69,41 @@ for (int i = 0; i < array.Length; i++)
 }
 
 Console.WriteLine($"в массиве:  [ {String.Join("; ", array)}] количество чётных чисел = {Res}");
-
+*/
 /*
 Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
+*/
 
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resArray = new int[size];
+    for (int i=0; i< resArray.Length; i++)
+    {
+        resArray[i] =new Random().Next(minValue, maxValue + 1);
+
+    }
+    return resArray;
+}
+
+int[] array = GetArray(6, -10, 10);
+
+Console.WriteLine($"Массив: [ {String.Join("; ", array)}]");
+int sum = 0;
+for (int i = 1; i < array.Length; i = i+2)
+{
+   // if (( array[i]) / 2  != 0 )
+   // {
+       sum = sum + array[i];
+   // }
+
+}
+//return sum;
+Console.WriteLine(sum);
+
+
+/*
 Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 */
