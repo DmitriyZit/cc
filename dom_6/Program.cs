750c6aa9955548bb6829e998b63cdbccee73968e
DmitriyZit <adima74@gmail.com>
2023-06-10 22:33:54 +0300

    Задача 43

--- dom_6/Program.cs
+++ dom_6/Program.cs
@@ -4,14 +4,14 @@
 
 
 Console.Write("Введите нижнюю границу массива : ");
-int minValue = Convert.ToInt32(Console.ReadLine());
+double minValue = Convert.ToInt32(Console.ReadLine());
 
     
 Console.Write("Введите верхнюю границу массива : ");
-int maxValue = Convert.ToInt32(Console.ReadLine());
+double maxValue = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите длину массива : ");
-int size = Convert.ToInt32(Console.ReadLine());
+double size = Convert.ToInt32(Console.ReadLine());
 
 
 if (minValue <100 || minValue > 1000)
@@ -30,10 +30,10 @@ if (minValue <100 || minValue > 1000)
         }
     
 
-int[] GetArray(int size, int minValue, int maxValue)
+double[] GetArray(double size, double minValue, double maxValue)
 {
-    int[] resArray = new int[size];
-    for (int i=0; i< resArray.Length; i++)
+    double[] resArray = new double[size];
+    for (double i=0; i< resArray.Length; i++)
 */
 
  //   Задача 39: Напишите программу, 
@@ -41,11 +41,11 @@ int[] GetArray(int size, int minValue, int maxValue)
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 /*
- int[] GetArray(int size, int min, int max)
+ double[] GetArray(double size, double min, double max)
  {
-     int[] array = new int[size];
+     double[] array = new double[size];
 
-     for (int i = 0; i < size; i++)
+     for (double i = 0; i < size; i++)
      {
          array[i] = new Random().Next(min, max + 1);
      }
@@ -53,28 +53,28 @@ int[] GetArray(int size, int minValue, int maxValue)
      return array;
  }
 
- int[] resultArray = GetArray(5, 0, 10); // size = 10, min = 0, max = 10
+ double[] resultArray = GetArray(5, 0, 10); // size = 10, min = 0, max = 10
  Console.WriteLine($"Исходный массив: [{String.Join("; ", resultArray)}]");
 
  // Способ №1. Через резервный массив 
- int[] CopyReverseArray(int[] arr)
+ double[] CopyReverseArray(double[] arr)
  {
-     int[] reverseArr = new int[arr.Length]; // размер копии = размер ориг. массива
-     for (int left = 0, right = arr.Length - 1; left < reverseArr.Length; left++, right--)
+     double[] reverseArr = new double[arr.Length]; // размер копии = размер ориг. массива
+     for (double left = 0, right = arr.Length - 1; left < reverseArr.Length; left++, right--)
      {
          reverseArr[left] = arr[right]; // reverseArr[0] = arr[1], reverseArr[1] = arr[0]
      }
      return reverseArr;
  }
- int[] reverse = CopyReverseArray(resultArray); // CopyReverseArray(resultArray)
+ double[] reverse = CopyReverseArray(resultArray); // CopyReverseArray(resultArray)
  Console.WriteLine($"Перевернутый массив: [{String.Join("; ", reverse)}]");
 
  // Способ №2. Через вспом. переменную 
- void ReverseArray(int[] arr)
+ void ReverseArray(double[] arr)
  {
-     for (int i = 0, j = arr.Length - 1; i < arr.Length / 2; i++, j--)
+     for (double i = 0, j = arr.Length - 1; i < arr.Length / 2; i++, j--)
      {
-         int temp = arr[i]; // Для массива arr[10,20,30]; temp = arr[0] => temp = 10
+         double temp = arr[i]; // Для массива arr[10,20,30]; temp = arr[0] => temp = 10
          arr[i] = arr[j]; // arr[0] = arr[2]: [arr[2],20,arr[2]] => [30,20,30];
          arr[j] = temp; // [30,20,temp] => temp = 10 => [30,20,10]
      }
@@ -86,14 +86,14 @@ int[] GetArray(int size, int minValue, int maxValue)
  /*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3
-*/
-Console.Write("Введите колличество чисел : ");
-int numberOfnumbers = Convert.ToInt32(Console.ReadLine());
-int[] GetArray(int size)
- {
-     int[] array = new int[numberOfnumbers];
 
-     for (int i = 0; i < numberOfnumbers; i++)
+Console.Write("Введите колличество чисел : ");
+double numberOfnumbers = Convert.ToInt32(Console.ReadLine());
+double[] GetArray(double size)
+ {
+     double[] array = new double[numberOfnumbers];
+
+     for (double i = 0; i < numberOfnumbers; i++)
      {
          Console.Write("Введите число : ");
          array[i] = Convert.ToInt32(Console.ReadLine());
@@ -102,14 +102,14 @@ int[] GetArray(int size)
 
      return array;
  }
-int[] resultArray = GetArray(numberOfnumbers);
+double[] resultArray = GetArray(numberOfnumbers);
 Console.WriteLine($"массив: [{String.Join("; ", resultArray)}]");
 
-//int Positiv()
+//double Positiv()
  //{
-    int morezero = 0;
-    int lesszero = 0;
-    for (int i = 0; i < numberOfnumbers; i++)
+    double morezero = 0;
+    double lesszero = 0;
+    for (double i = 0; i < numberOfnumbers; i++)
      {
         if(resultArray[i] > 0)
         {
@@ -122,11 +122,28 @@ Console.WriteLine($"массив: [{String.Join("; ", resultArray)}]");
    
 Console.WriteLine("Число положительных чисел = " + morezero  + "  Число отрицательных = " + lesszero);
 
+*/
 
 
 
-
-/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
+
+Console.Write("Введите координаты b1 : ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты k1 : ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты b2 : ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты k2 : ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = 0;
+double y = 0;
+x = (b2-b1) / (k1-k2);
+y = k1 * (b2-b1) / (k1 - k2) + b1;
+Console.WriteLine("Точка пересечения x = " + Math.Round(x, 2)  + "  y = " + Math.Round(y, 2) );
+
+
