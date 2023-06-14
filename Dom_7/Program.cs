83170a54b5898c6c56324749e23a9032902d3afd
DmitriyZit <adima74@gmail.com>
2023-06-14 17:36:36 +0300

    Домашнее задание 7

--- Dom_7/Program.cs
+++ Dom_7/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Write("Введите нижнюю границу массива : ");
+double minValue = Convert.ToInt32(Console.ReadLine());
+
+    
+Console.Write("Введите верхнюю границу массива : ");
+double maxValue = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(double[,] matr)
+{
+    //int[,] matrix = new int[3, 4];
+    //matrix[1, 1] = 55;
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+     for (int j = 0; j < 4; j++)
+     {
+        Console.Write($"{matr[i, j]},");
+     }
+    Console.WriteLine();
+    }
+}
+void FillArray(double[,] matr)
+{
+    //int[,] matrix = new int[3, 4];
+    //matrix[1, 1] = 55;
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+         matr[i,j] = new Random().NextDouble(1,10); //Random().Next(1,10);
+        // Console.Write($"{matrix[i, j]},");
+        }
+    //Console.WriteLine();
+    }
+}
+double[,] matrix = new double[minValue, maxValue];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+/*Задача 50. Напишите программу, 
+которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+
+/*Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Дополнительное:
+
+Задайте двумерный массив. Найдите сумму элементов, 
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
