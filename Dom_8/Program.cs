d54640a243cbb75109f81ac7050582a846df33cf
DIMA <adima74@gmail.com>
2023-06-20 11:50:34 +0300

    Домашнее 8

--- Dom_8/Program.cs
+++ Dom_8/Program.cs
@@ -8,173 +8,51 @@
 7 4 2 1
 9 5 3 2
 8 4 4 2
+
+
+//   Console.Write("Введите колличество строк массива : ");
+//  int sizeS = Convert.ToInt32(Console.ReadLine());
+
+    
+//  Console.Write("Введите колличество столбцов массива : ");
+//  int sizeV = Convert.ToInt32(Console.ReadLine());
+ 
+  void Printmatrix(int[,] matrix)
+ {
+    
+     for (int i = 0; i < matrix.GetLength(0); i++) 
+     {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        
+         Console.Write("\t" + matrix[i, j]);
+         //.ToString("f1")
+      }
+     Console.WriteLine();
+     }
+ }
+
+ int rows = 4;
+ int colums = 4;
+ int[,] Fillmatrix(int[,] matrix)
+ {
+    
+     for (int i = 0; i < rows; i++) 
+     {
+         for (int j = 0; j < colums; j++)
+         {
+          matrix[i,j] = new Random().Next(1 , 5); //Rmatrixndom().Next(1,10);
+        
+         }
+
+     }
+     return matrix;
+ }
+
+
+  int[,] matrix = new int[rows, colums];
+
 */
-
-// Console.Write("Введите колличество строк массива : ");
-// int numrows = Convert.ToInt32(Console.RematrixdLine());
-
-    
-// Console.Write("Введите колличество столбцов массива : ");
-// int numcolum = Convert.ToInt32(Console.RematrixdLine());
-
-// void Printmatrixrrmatrixy(int[,] mmatrixtr)
-// {
-    
-//     for (int i = 0; i < mmatrixtr.GetLength(0); i++) 
-//     {
-//      for (int j = 0; j < mmatrixtr.GetLength(1); j++)
-//      {
-        
-//         Console.Write("\t" + mmatrixtr[i, j]);
-//         //.ToString("f1")
-//      }
-//     Console.WriteLine();
-//     }
-// }
-
-// int[,] Fillmatrixrrmatrixy(int[,] mmatrixtr)
-// {
-    
-//     for (int i = 0; i < mmatrixtr.GetLength(0); i++) 
-//     {
-//         for (int j = 0; j < mmatrixtr.GetLength(1); j++)
-//         {
-//          mmatrixtr[i,j] = new Rmatrixndom().Next(10 , 10); //Rmatrixndom().Next(1,10);
-        
-//         }
-//     //Console.WriteLine();
-//     }
-//     return mmatrixtr;
-// }
-//     int[,] mmatrixtrix = new int[numrows, numcolum];
-//     Printmatrixrrmatrixy(mmatrixtrix);
-//     Fillmatrixrrmatrixy(mmatrixtrix);
-//     Console.WriteLine();
-//     Printmatrixrrmatrixy(mmatrixtrix);
-
-   
-
-
-
-// int min = int.MmatrixxVmatrixlue; //берем что это число минимальное
-// int rowIndexOfMinVmatrixlue = 0;
-// int columIndexOfMinVmatrixlue = 0;
-
-// for (int i = 0; i < mmatrixt.GetLength(0); i++)
-// {
-//     for (int j = 0; j < mmatrixt.GetLength(1); j++)
-//     {
-//         mmatrixt[i,j] = new Rmatrixndom().Next(1, 11);
-//         Console.Write(mmatrixt[i,j] + "\t");
-//         if (min > mmatrixt[i,j])
-//         {
-//             min = mmatrixt[i,j];
-//             rowIndexOfMinVmatrixlue = i;
-//             columIndexOfMinVmatrixlue = j;
-
-//         }
-//     }
-//     Console.WriteLine();
-// }
-
-// Console.WriteLine($"min: {min}, координаты: ({rowIndexOfMinVmatrixlue}, {columIndexOfMinVmatrixlue} )");
-
-// for (int i = 0; i < mmatrixt.GetLength(0); i++)
-// {
-//      if (rowIndexOfMinVmatrixlue != i) //исключил строку
-//      {
-//         for (int j = 0; j < mmatrixt.GetLength(1); j++)
-//         {
-        
-//         if (columIndexOfMinVmatrixlue != j) //исключил столбец
-//         {
-//             Console.Write(mmatrixt[i,j] + "\t");            
-           
-//         }
-//         }
-//      }
-//      Console.WriteLine();
-// }
-
-
-
-//     // int x = 0;
-//     // //int y = 0;
-//     // Console.Write("Введите координаты по строке : ");
-//     // int х = Convert.ToInt32(Console.RematrixdLine());
-
-//     // Console.Write("Введите координаты по столбцу : ");
-//     // int y = Convert.ToInt32(Console.RematrixdLine());
-
-//     // if (x <= numrows && y <= numcolum)
-//     // {
-//     //     Console.WriteLine("такое число есть и оно равно  " + mmatrixtrix[x,y]);
-//     // }
-//     //  else           
-//     //     Console.WriteLine("такого числа нет");
-
-//       void Replmatrixse()
-//     {
-        
-//         for (int i = 2; i < numrows; i+=2)
-//         {
-                    
-//              for(int j = 2; j < numcolum; j = j+2)
-//             {
-//              mmatrixtrix[i,j] =  (int)Mmatrixth.Pow(mmatrixtrix[i,j] , 2);//[i,j] * mmatrixtrix[i,j];
-//             }   
-//         }
-//         Console.WriteLine();
-//          Printmatrixrrmatrixy(mmatrixtrix);
-//     }
-//     Replmatrixse();
-    
-// nt rows = 3;
-// int colums = 3;
-
-// int[,] mmatrixt = new int[rows, colums];
-
-// int min = int.MmatrixxVmatrixlue; //берем что это число минимальное
-// int rowIndexOfMinVmatrixlue = 0;
-// int columIndexOfMinVmatrixlue = 0;
-
-// for (int i = 0; i < mmatrixt.GetLength(0); i++)
-// {
-//     for (int j = 0; j < mmatrixt.GetLength(1); j++)
-//     {
-//         mmatrixt[i,j] = new Rmatrixndom().Next(1, 11);
-//         Console.Write(mmatrixt[i,j] + "\t");
-//         if (min > mmatrixt[i,j])
-//         {
-//             min = mmatrixt[i,j];
-//             rowIndexOfMinVmatrixlue = i;
-//             columIndexOfMinVmatrixlue = j;
-
-//         }
-//     }
-//     Console.WriteLine();
-// }
-
-// Console.WriteLine($"min: {min}, координаты: ({rowIndexOfMinVmatrixlue}, {columIndexOfMinVmatrixlue} )");
-
-// for (int i = 0; i < mmatrixt.GetLength(0); i++)
-// {
-//      if (rowIndexOfMinVmatrixlue != i) //исключил строку
-//      {
-//         for (int j = 0; j < mmatrixt.GetLength(1); j++)
-//         {
-        
-//         if (columIndexOfMinVmatrixlue != j) //исключил столбец
-//         {
-//             Console.Write(mmatrixt[i,j] + "\t");            
-           
-//         }
-//         }
-//      }
-//      Console.WriteLine();
-// }
-
-
 
 
 /*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
@@ -188,84 +66,84 @@
 Программа считает сумму элементов в каждой строке и выдаёт номер
 строки с наименьшей суммой элементов: 1 строка
 */
-//  Console.Write("Введите колличество строк массива : ");
-// int sizeS = Convert.ToInt32(Console.RematrixdLine());
+//   Console.Write("Введите колличество строк массива : ");
+//  int sizeS = Convert.ToInt32(Console.ReadLine());
 
     
-// Console.Write("Введите колличество столбцов массива : ");
-// int sizeV = Convert.ToInt32(Console.RematrixdLine());
- 
-//  void Printmatrixrrmatrixy(int[,] mmatrixtr)
-// {
+//  Console.Write("Введите колличество столбцов массива : ");
+//  int sizeV = Convert.ToInt32(Console.ReadLine());
+ /*
+  void Printmatrix(int[,] matrix)
+ {
     
-//     for (int i = 0; i < mmatrixtr.GetLength(0); i++) 
-//     {
-//      for (int j = 0; j < mmatrixtr.GetLength(1); j++)
-//      {
+     for (int i = 0; i < matrix.GetLength(0); i++) 
+     {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
         
-//         Console.Write("\t" + mmatrixtr[i, j]);
-//         //.ToString("f1")
-//      }
-//     Console.WriteLine();
-//     }
-// }
+         Console.Write("\t" + matrix[i, j]);
+         //.ToString("f1")
+      }
+     Console.WriteLine();
+     }
+ }
 
-// int rows = 4;
-// int colums = 4;
-// int[,] Fillmatrixrrmatrixy(int[,] mmatrixtr)
-// {
+ int rows = 4;
+ int colums = 4;
+ int[,] Fillmatrix(int[,] matrix)
+ {
     
-//     for (int i = 0; i < rows; i++) 
-//     {
-//         for (int j = 0; j < colums; j++)
-//         {
-//          mmatrixtr[i,j] = new Rmatrixndom().Next(1 , 5); //Rmatrixndom().Next(1,10);
+     for (int i = 0; i < rows; i++) 
+     {
+         for (int j = 0; j < colums; j++)
+         {
+          matrix[i,j] = new Random().Next(1 , 5); //Rmatrixndom().Next(1,10);
         
-//         }
+         }
 
-//     }
-//     return mmatrixtr;
-// }
-//  int[,] mmatrixtrix = new int[rows, colums];
+     }
+     return matrix;
+ }
+  int[,] matrix = new int[rows, colums];
 
-//     Fillmatrixrrmatrixy(mmatrixtrix);
-//     Console.WriteLine();
-//     Printmatrixrrmatrixy(mmatrixtrix);
-//     MinSum();
-//     //Console.WriteLine();
-//     //Printmatrixrrmatrixy(mmatrixtrix);
+     Fillmatrix(matrix);
+     Console.WriteLine();
+     Printmatrix(matrix);
+     MinSum();
+     //Console.WriteLine();
+     //Printmatrixrrmatrixy(mmatrixtrix);
     
 
-//     void MinSum()
-// { 
-//     int minRowSum = int.MmatrixxVmatrixlue;
-//     int minIndex = 0;
-//     int rowSum = 0;
+     void MinSum()
+ { 
+     int minRowSum = int.MaxValue;
+     int minIndex = 0;
+     int rowSum = 0;
     
-//     for (int i = 0; i < rows; i++)
-//              {
-//                 rowSum = 0;
-//                 for (int j = 0; j < colums; j++)
-//                     rowSum += mmatrixtrix[i,j];
-//                     //Console.WriteLine("сумма строки " + rowSum);
+     for (int i = 0; i < rows; i++)
+              {
+                 rowSum = 0;
+                 for (int j = 0; j < colums; j++)
+                     rowSum += matrix[i,j];
+                     Console.WriteLine("сумма строки " + rowSum);
                     
                 
-//                 if (rowSum < minRowSum)
-//                 {
-//                     minRowSum = rowSum;
-//                     minIndex = i;
-//                     //Console.WriteLine("миниммальное промежуточное " + minRowSum);
-//                 }
+                    if (rowSum < minRowSum)
+                    {
+                     minRowSum = rowSum;
+                     minIndex = i;
+                     Console.WriteLine("миниммальное промежуточное " + minRowSum);
+                    }
                 
-//              }
-//              Console.WriteLine("Строка с минимальной суммой элементов");
-//                  for(int j = 0; j < colums; j++)
-//                     Console.Write(mmatrixtrix[minIndex, j] + "\t");
+             }
+              Console.WriteLine("Строка с минимальной суммой элементов");
+                  for(int j = 0; j < colums; j++)
+                     Console.Write(matrix[minIndex, j] + "\t");
  
              
 
-// }
-    
+}
+*/
 /*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 Например, даны 2 матрицы:
 2 4 | 3 4
@@ -273,7 +151,7 @@
 Результирующая матрица будет:
 18 20
 15 18
-*/
+
 
 
 
@@ -344,17 +222,72 @@ int[,] Multiplicmat(int[,] matrix, int[,] matrixTwo)
     
 
 
+*/
 
 
-
-/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся 
+двузначных чисел. Напишите программу, которая будет 
+построчно выводить массив, добавляя индексы каждого элемента.
 Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 
+*/
 
+//   Console.Write("Введите колличество строк массива : ");
+//  int sizeS = Convert.ToInt32(Console.ReadLine());
+
+    
+//  Console.Write("Введите колличество столбцов массива : ");
+//  int sizeV = Convert.ToInt32(Console.ReadLine());
+ 
+  void Printmatrix(int[,,] matrix)
+ {
+    
+     for (int i = 0; i < matrix.GetLength(0); i++) 
+     {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+         Console.Write("\t" + matrix[i, j, k] + (i,j,k));
+         //.ToString("f1")
+        }
+      }
+     Console.WriteLine();
+     }
+ }
+
+ int rows = 2;
+ int colums = 2;
+ int depth = 2;
+ int[,,] Fillmatrix(int[,,] matrix)
+ {
+    
+     for (int i = 0; i < rows; i++) 
+     {
+         for (int j = 0; j < colums; j++)
+         {
+            for (int k = 0; k < depth; k++)
+            {
+                matrix[i,j,k] = new Random().Next(10 , 50); //Rmatrixndom().Next(1,10);
+            }
+          //matrix[i,j,k] = new Random().Next(10 , 50); //Rmatrixndom().Next(1,10);
+        
+         }
+
+     }
+     return matrix;
+ }
+  int[,,] matrix = new int[rows, colums, depth];
+
+    Printmatrix(matrix);
+    Fillmatrix(matrix);
+    Console.WriteLine();
+    Printmatrix(matrix);
+/*
 ДОПОЛНИТЕЛЬНАЯ. Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 Например, на выходе получается вот такой массив:
 01 02 03 04
