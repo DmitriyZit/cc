a14d7e5c8a272c3e0ab6f935a903803cefc90be7
DIMA <adima74@gmail.com>
2023-06-23 14:06:38 +0300

    задача 68

--- Dom9/Program.cs
+++ Dom9/Program.cs
@@ -19,7 +19,7 @@ Console.WriteLine(GetNunbersString(n, 1));
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-
+/*
 Console.Write("Введите число : ");
 int m = Convert.ToInt32(Console.ReadLine());
 
@@ -32,27 +32,27 @@ int SumWithin(int start, int end)
     return start + SumWithin(start + 1 , end);
 }
 Console.WriteLine(SumWithin(m , n));
-
+*/
 
 // Задача 68: Напишите программу вычисления функции Аккермана
 // с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+Console.Write("Введите число : ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
 
 
-
-// Console.Write("Введите число : ");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-
-// // Console.Write("Введите колличество столбцов массива : ");
-// // int colums = Convert.ToInt32(Console.ReadLine());
-
-// int Factorial(int number)
-// {
-//     if (number == 1) return 1;
-//     return number * Factorial(number - 1);
-// }
-
-// Console.WriteLine($"Факториал числа {n} = {Factorial(n)}" );
+int Akkermann(int m, int n)
+ {
+            
+            if (m == 0)
+                return n + 1;
+            if (n == 0)
+                return Akkermann(m - 1, 1);
+            return Akkermann(m - 1, Akkermann(m, n - 1));
+}
+Console.WriteLine(Akkermann(m , n));
