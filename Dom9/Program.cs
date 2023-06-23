4e56a562ed8e2b6d73f12bc82be6c31edc66d700
DIMA <adima74@gmail.com>
2023-06-23 13:51:56 +0300

    задача 66

--- Dom9/Program.cs
+++ Dom9/Program.cs
@@ -2,7 +2,7 @@
 // выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
+/*
 Console.Write("Введите число : ");
 int n = Convert.ToInt32(Console.ReadLine());
 
@@ -13,14 +13,25 @@ string GetNunbersString(int start, int end)
 
 }
 Console.WriteLine(GetNunbersString(n, 1));
-
+*/
 
 // Задача 66: Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Write("Введите число : ");
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumWithin(int start, int end)
+{
+    if(start == end) return start;
+    return start + SumWithin(start + 1 , end);
+}
+Console.WriteLine(SumWithin(m , n));
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана
